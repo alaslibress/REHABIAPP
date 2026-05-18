55a85bfb8313121d7e209ab246b5d8b17f319b91
alaslibress <jandroman23@gmail.com>
2026-05-18 08:00:42 +0200

    actualizado el estado de api y data para que sea igual que los que hay en aws, hice cambios en las de aws que no implemente en local

--- api/target/generated-sources/annotations/com/rehabiapp/api/application/mapper/SanitarioMapperImpl.java
+++ api/target/generated-sources/annotations/com/rehabiapp/api/application/mapper/SanitarioMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-17T22:12:49+0200",
+    date = "2026-05-18T07:58:12+0200",
     comments = "version: 1.6.3, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
 )
 @Component
